98771d629c7ca6686cc03d44e4d9a5ee20189f13
kripalsingh1 <kripalsinghrj@gmail.com>
2025-08-03 09:52:22 +0530

    Add files via upload

---
+++ webhook.py
@@ -0,0 +1,19 @@
+
+from flask import Flask, request
+import requests
+
+app = Flask(__name__)
+
+BOT_TOKEN = "7962131421:AAFgZruC54XSLbTI0r7-TvNUF72hc151P0E"
+CHAT_ID = "5933740363"
+
+@app.route('/', methods=['POST'])
+def send_telegram():
+    data = request.json
+    message = data.get("message", "No message received")
+
+    telegram_url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+    payload = {"chat_id": CHAT_ID, "text": message}
+    requests.post(telegram_url, data=payload)
+
+    return 'OK', 200
